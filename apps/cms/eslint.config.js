f07b152bb9135840ce984d6afc34bec66b9598d1
RevealUI Studio <founder@revealui.com>
2026-01-01 02:01:21 -0500

    fix(lint): ignore .next/ build artifacts in ESLint configs
    - Convert ESLint configs to flat config array format
    - Add **/.next/** to ignores in both cms and web apps
    - Fix trailing comma in dev package.json
    
    This reduces lint errors from 442 to 0 by preventing ESLint from
    processing Next.js build artifacts in the .next/ directories.

--- apps/cms/eslint.config.js
+++ apps/cms/eslint.config.js
@@ -1,4 +1,9 @@
 import sharedConfig from "../../packages/dev/src/eslint/eslint.config.js";
-export default {
-  ...sharedConfig,
-};
+export default [
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/.turbo/**", "**/.next/**"],
+  },
+  {
+    ...sharedConfig,
+  },
+];
--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,5 +1,9 @@
 import sharedConfig from "../../packages/dev/src/eslint/eslint.config.js";
-export default {
-  ...sharedConfig,
-  ignores: ["dist/", "node_modules/", ".turbo", "public/"],
-};
+export default [
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/.turbo/**", "**/public/**", "**/.next/**"],
+  },
+  {
+    ...sharedConfig,
+  },
+];
