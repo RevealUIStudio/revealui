b2c00cb7d17a92c615856a4e484dd05c8970f9b1
RevealUI Studio <founder@revealui.com>
2026-01-28 23:57:21 -0500

    ga

--- apps/dashboard/src/components/DashboardLayout.tsx
+++ apps/dashboard/src/components/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+import { Group, Panel, Separator } from 'react-resizable-panels'
 import { AgentPanel } from './AgentPanel'
 import { ContentPanel } from './ContentPanel'
 import { DataPanel } from './DataPanel'
@@ -8,31 +8,31 @@ import { DataPanel } from './DataPanel'
 export function DashboardLayout() {
   return (
     <div className="h-screen bg-gray-900 text-white overflow-hidden">
-      <PanelGroup direction="horizontal">
+      <Group orientation="horizontal">
         {/* Left Panel - Agent Management (25%) */}
         <Panel defaultSize={25} minSize={20} maxSize={35}>
           <AgentPanel />
         </Panel>
 
-        <PanelResizeHandle className="w-1 bg-gray-700 hover:bg-blue-500 transition-colors" />
+        <Separator className="w-1 bg-gray-700 hover:bg-blue-500 transition-colors" />
 
         {/* Right Panel - Content & Data (75%) */}
         <Panel defaultSize={75} minSize={65}>
-          <PanelGroup direction="vertical">
+          <Group orientation="vertical">
             {/* Top Panel - Live Preview/Content */}
             <Panel defaultSize={60} minSize={40}>
               <ContentPanel />
             </Panel>
 
-            <PanelResizeHandle className="h-1 bg-gray-700 hover:bg-blue-500 transition-colors" />
+            <Separator className="h-1 bg-gray-700 hover:bg-blue-500 transition-colors" />
 
             {/* Bottom Panel - Data Visualization */}
             <Panel defaultSize={40} minSize={25}>
               <DataPanel />
             </Panel>
-          </PanelGroup>
+          </Group>
         </Panel>
-      </PanelGroup>
+      </Group>
     </div>
   )
 }
